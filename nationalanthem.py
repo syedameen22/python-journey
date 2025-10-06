6de5167872708b8dc8b668893e328b8726b093f5
syedameen22 <ameensyed10x@gmail.com>
2025-10-06 13:20:08 +0530

    Create nationalanthem.py

---
+++ nationalanthem.py
@@ -0,0 +1,17 @@
+print('''Jana-gana-mana-adhinayaka jaya he
+Bharata-bhagya-vidhata
+Panjaba-Sindh-Gujarata-Maratha
+Dravida-Utkala-Banga
+
+
+Vindhya-Himachala-Yamuna-Ganga
+Uchchala-jaladhi-taranga
+Tava Subha name jage
+Tava subha asisa mage
+Gahe tava jaya-gatha
+
+
+Jana-gana-mangala-dayaka jaya he
+Bharata-bhagya-vidhata
+Jaya he, Jaya he, Jaya he
+Jaya jaya jaya jaya he''')
