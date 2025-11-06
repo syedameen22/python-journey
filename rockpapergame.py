db440f72b258c7893961c908375dc49880b2536e
syedameen22 <ameensyed10x@gmail.com>
2025-11-06 15:41:53 +0530

    rock

--- rockpapergame.py
+++ rockpapergame.py
@@ -1,10 +1,10 @@
 import random
 computer=random.choice([-1,0,1])
 youstr=input("enter your choise :")
-youDict={"rock": 1,"paper": -1,"scissor": 0}
-reversedict={1: "rock" , -1: "paper" , 0: "scissor"}
+youDict={"r": 1,"p": -1,"s": 0}
+revesedict={1: "rock" , -1: "paper" , 0: "scissor"}
 you=youDict[youstr]
-print(f"enter your choise : {reversedict[you]}\n computers choise : {reversedict[computer]}")
+print(f"enter your choise : {reversedict[you]}/n computers choise : r{reversedict[computer]}")
 if(computer==you):
     print("match draw")
 else:
@@ -16,9 +16,9 @@ else:
         print("you loose")
     elif(computer==1 and you==0):
         print("you win ")
-    elif(computer==0 and you==-1):
-        print("you win")
     elif(computer==0 and you==1):
         print("you loose")
+    elif(computer==0 and you==-1):
+        print("you win")
     else:
         print("somthing went wrong")
